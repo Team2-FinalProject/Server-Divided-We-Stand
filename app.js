78fae711493e3b774aecf61308cc4cd6d0ceb2c8
rivari22 <muchammadrvr22@gmail.com>
2020-12-17 21:59:19 +0700

    update score

--- app.js
+++ app.js
@@ -11,22 +11,21 @@ let rooms = [];
 let onlineUsers = [];
 let activeRoom = [];
 
-let playerOne = [] // feet
-let playerTwo = [] // hip
+let playerOne = []; // feet
+let playerTwo = []; // hip
 
 //team Two
-let playerThree = [] // feet
-let playerFour = [] // hip
-
+let playerThree = []; // feet
+let playerFour = []; // hip
 
 io.on("connection", (socket) => {
   console.log("a user connected");
   // game
-  
+
   // socket.emit("hello", "HELLO WORLD")
   // io.emit("gamePlayer", gamePlayers)
   // io.on('hello', )
-  
+
   // socket.on('moveBone', (payload) => {
   //   if(payload.team === "teamOne") {
   //     let user = gamePlayers.teamOne.find(e => e.username === payload.username)
@@ -43,7 +42,6 @@ io.on("connection", (socket) => {
 
   //end game
 
-
   socket.on("createRoom", (payload) => {
     let room = {
       id: payload.id,
@@ -57,13 +55,13 @@ io.on("connection", (socket) => {
       scoreTeamTwo: 0,
     };
     rooms.push(room);
-    activeRoom.push(room)
+    activeRoom.push(room);
     io.emit("createRoom", rooms);
   });
 
   socket.on("joinRoom", (payload) => {
     socket.join(payload.roomName, () => {
-      const roomFind = activeRoom.find((e) => e.id === payload.idRoom)
+      const roomFind = activeRoom.find((e) => e.id === payload.idRoom);
       // const playerData = {
       //   username: payload.username,
       //   cordinatX: null,
@@ -71,7 +69,7 @@ io.on("connection", (socket) => {
       //   roomName: payload.roomName
       // }
       if (payload.team === "teamOne") {
-        roomFind.teamOne.push(payload.username)
+        roomFind.teamOne.push(payload.username);
         // roomFind.teamOne[0] ? playerTwo.push(playerData) : playerOne.push(playerData)
       } else if (payload.team === "teamTwo") {
         roomFind.teamTwo.push(payload.username);
@@ -89,53 +87,65 @@ io.on("connection", (socket) => {
     // console.log(payload)
     let room = activeRoom.find((e) => e.id === payload.id);
     room.status = true;
-    console.log(playerOne, "<<<" , room.teamOne[0])
+    console.log(playerOne, "<<<", room.teamOne[0]);
 
-    room.teamOne[0] && playerOne.push({
-      username: room.teamOne[0],
-      cordinatX: null,
-      cordinatY: null,
-      roomName: room.name,
-      role: 'feet',
-      team: 'teamOne'
-    }) 
-    room.teamOne[1] && playerTwo.push({
-      username: room.teamOne[1],
-      cordinatX: null,
-      cordinatY: null,
-      roomName: room.name,
-      role: 'hip',
-      team: 'teamOne'
-    })
-    room.teamTwo[0] && playerThree.push({
-      username: room.teamTwo[0],
-      cordinatX: null,
-      cordinatY: null,
-      roomName: room.name,
-      role: 'feet',
-      team: 'teamTwo'
-    }) 
-    room.teamTwo[1] && playerFour.push({
-      username: room.teamTwo[1],
-      cordinatX: null,
-      cordinatY: null,
-      roomName: room.name,
-      role: 'hip',
-      team: 'teamTwo'
-    })
+    room.teamOne[0] &&
+      playerOne.push({
+        username: room.teamOne[0],
+        cordinatX: null,
+        cordinatY: null,
+        roomName: room.name,
+        role: "feet",
+        team: "teamOne",
+      });
+    room.teamOne[1] &&
+      playerTwo.push({
+        username: room.teamOne[1],
+        cordinatX: null,
+        cordinatY: null,
+        roomName: room.name,
+        role: "hip",
+        team: "teamOne",
+      });
+    room.teamTwo[0] &&
+      playerThree.push({
+        username: room.teamTwo[0],
+        cordinatX: null,
+        cordinatY: null,
+        roomName: room.name,
+        role: "feet",
+        team: "teamTwo",
+      });
+    room.teamTwo[1] &&
+      playerFour.push({
+        username: room.teamTwo[1],
+        cordinatX: null,
+        cordinatY: null,
+        roomName: room.name,
+        role: "hip",
+        team: "teamTwo",
+      });
 
-    let findPlayerOne = playerOne.find(e => e.username === room.teamOne[0])
-    let findPlayerTwo = playerTwo.find(e => e.username === room.teamOne[1])
-    let findPlayerThree = playerThree.find(e => e.username === room.teamTwo[0])
-    let findPlayerFour = playerFour.find(e => e.username === room.teamTwo[1])
-    console.log(findPlayerOne,findPlayerTwo, findPlayerThree, findPlayerFour, "find player" )
+    let findPlayerOne = playerOne.find((e) => e.username === room.teamOne[0]);
+    let findPlayerTwo = playerTwo.find((e) => e.username === room.teamOne[1]);
+    let findPlayerThree = playerThree.find(
+      (e) => e.username === room.teamTwo[0]
+    );
+    let findPlayerFour = playerFour.find((e) => e.username === room.teamTwo[1]);
+    console.log(
+      findPlayerOne,
+      findPlayerTwo,
+      findPlayerThree,
+      findPlayerFour,
+      "find player"
+    );
     let data = {
       room,
       playerOne: findPlayerOne,
       playerTwo: findPlayerTwo,
       playerThree: findPlayerThree,
-      playerFour: findPlayerFour
-    }
+      playerFour: findPlayerFour,
+    };
 
     // io.in(room.name).emit("startGame", data);
     io.emit("startGame", data);
@@ -179,10 +189,58 @@ io.on("connection", (socket) => {
   //   io.emit('hai', 'hai juga from server to game')
   // })
 
-  socket.on('moveBone', data => {
+  socket.on("moveBone", (data) => {
+    // console.log(playerOne )
+    // console.log(playerTwo )
+    // console.log(playerThree )
+    // console.log(playerFour )
     // console.log(data)
-    socket.broadcast.emit('updateBone', data)
-  })
+
+    let user;
+    let findPlayerOne = playerOne.find((e) => e.username === data.player);
+    let findPlayerTwo = playerTwo.find((e) => e.username === data.player);
+    let findPlayerThree = playerThree.find((e) => e.username === data.player);
+    let findPlayerFour = playerFour.find((e) => e.username === data.player);
+
+    if (findPlayerOne) {
+      user = findPlayerOne.username === data.player && findPlayerOne;
+    } else if (findPlayerTwo) {
+      user = findPlayerTwo.username === data.player && findPlayerTwo;
+    } else if (findPlayerThree) {
+      user = findPlayerThree.username === data.player && findPlayerThree;
+    } else if (findPlayerFour) {
+      user = findPlayerFour.username === data.player && findPlayerFour;
+    }
+
+    user.cordinatX = data.x;
+    user.cordinatY = data.y;
+    // console.log(user, ",ini user")
+    let payload = {
+      room: data.room,
+      user: user,
+    };
+    // console.log(payload, "ini data yg akan dikirim")
+    socket.broadcast.emit("updateBone", payload);
+  });
+
+  socket.on("score", (payload) => {
+    let room = activeRoom.find((e) => e.id === payload.room.id);
+    if (payload.team === "teamOne") {
+      console.log(room, "ini room pl 1");
+      room.scoreTeamOne = payload.score;
+    } else {
+      room.scoreTeamTwo = payload.score;
+    }
+    console.log(room, "ini room after add pl 1");
+  });
+
+  // socket.on('scoreP2', payload => {
+  //   let room = activeRoom.find((e) => e.id === payload.room.id);
+  //   console.log(room, "ini room player 2")
+  //   room.scoreTeamTwo = payload.score
+  //   console.log(room, "ini room after add player 2")
+
+  // })
 
   // socket.on('goalP1', )
 
@@ -198,4 +256,4 @@ http.listen(port, () => {
   console.log(`listening on port ${port}`);
 });
 
-module.exports = app
+module.exports = app;
